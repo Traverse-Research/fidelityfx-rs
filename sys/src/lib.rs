83e66f74ba684de00b932f126fd0a299060df567
Marijn Suijten <marijn@traverseresearch.nl>
2025-10-14 12:06:08 +0200

    Default-enable all features and test in CI

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -5,41 +5,42 @@
 
 include!("bindings.rs");
 
+// TODO(Marijn): Use `ash` and `windows` to provide the right types for
+// Vulkan handles and D3D12 structures?
+
 #[cfg(feature = "vulkan")]
 pub mod vk {
     use crate::*;
 
     type VkPhysicalDevice = u64;
     type VkDevice = u64;
-    type VkInstance = u64;
     type VkCommandBuffer = u64;
     type PFN_vkGetDeviceProcAddr = *const std::ffi::c_void;
-    type PFN_vkGetInstanceProcAddr = *const std::ffi::c_void;
-    type PFN_vkEnumerateDeviceExtensionProperties = *const std::ffi::c_void;
-    type VkBuffer = u64;
-    type VkImage = u64;
-    type VkImageView = u64;
-    type VkFormat = i32;
-    type VkImageLayout = i32;
 
     include!("vk_bindings.rs");
 }
 
 #[cfg(feature = "dx12")]
 pub mod dx12 {
-
     use crate::*;
+
+    // Interfaces must be defined as c_void type (or other private structure) because the `windows`
+    // crate types internally define the pointer whereas C bindings refer to these opaque types with
+    // a pointer.
     type ID3D12CommandList = std::ffi::c_void;
     type ID3D12Device = std::ffi::c_void;
     type ID3D12Resource = std::ffi::c_void;
     type IDXGISwapChain4 = std::ffi::c_void;
     type ID3D12CommandQueue = std::ffi::c_void;
     type IDXGIFactory = std::ffi::c_void;
+
     type DXGI_SWAP_CHAIN_DESC = std::ffi::c_void;
     type DXGI_SWAP_CHAIN_DESC1 = std::ffi::c_void;
     type DXGI_SWAP_CHAIN_FULLSCREEN_DESC = std::ffi::c_void;
 
+    #[allow(clippy::upper_case_acronyms)]
     type UINT = u32;
+    #[allow(clippy::upper_case_acronyms)]
     type HWND = UINT;
     type DXGI_FORMAT = u32;
 
