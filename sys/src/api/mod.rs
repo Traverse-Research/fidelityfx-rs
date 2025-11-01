7150413f90497e30781b6af930dc791b0fe89010
Marijn Suijten <marijn@traverseresearch.nl>
2025-11-01 11:07:15 +0100

    Document the differences between the `sdk` and `api` module

--- sys/src/api/mod.rs
+++ sys/src/api/mod.rs
@@ -1,3 +1,7 @@
+//! Bindings to the new `ffx-api` abstraction, generated from <https://github.com/GPUOpen-LibrariesAndSDKs/FidelityFX-SDK/tree/v1.1.4/ffx-api>.
+//!
+//! See <https://github.com/GPUOpen-LibrariesAndSDKs/FidelityFX-SDK/blob/v1.1.4/docs/getting-started/ffx-api.md> for details on how to use this new API.
+
 #![allow(
     non_upper_case_globals,
     non_camel_case_types,
