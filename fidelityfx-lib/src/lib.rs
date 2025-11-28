d32a472e907a6daac61bbb8d020242c56dc3c372
Darius <darius1@live.nl>
2025-11-28 16:43:52 +0100

    feedback

--- fidelityfx-lib/src/lib.rs
+++ fidelityfx-lib/src/lib.rs
@@ -1,8 +1,10 @@
 use std::path::{Path, PathBuf};
 
 pub fn dll_path_vk() -> PathBuf {
-    Path::new(env!("CARGO_MANIFEST_DIR")).join("PrebuiltSignedDLL/amd_fidelityfx_vk.dll")
+    Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("PrebuiltSignedDLL/amd_fidelityfx_vk.dll")
 }
 pub fn dll_path_dx12() -> PathBuf {
-    Path::new(env!("CARGO_MANIFEST_DIR")).join("PrebuiltSignedDLL/amd_fidelityfx_dx12.dll")
+    Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("PrebuiltSignedDLL/amd_fidelityfx_dx12.dll")
 }
